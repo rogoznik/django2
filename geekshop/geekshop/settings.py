13556355a520b2f3486addb121582ea97c4c99f6
nikrogoz <nikrogoz@gmail.com>
2021-07-11 15:28:37 +0500

    homework 5

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-a(b@o61*2t7a57=879dwevs!@@gvd%#9s)!&v$z*z$)hhl*&!1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -88,10 +88,18 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'USER': 'geekuser'
     }
 }
 

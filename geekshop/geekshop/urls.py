17a0dcdcad3b455003c997107e1bb0d34eccc6c2
nikrogoz <nikrogoz@gmail.com>
2021-07-13 14:25:36 +0500

    homework 6

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from mainapp import urls
 from .views import contacts, main
 from django.conf import settings
@@ -37,3 +37,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+   import debug_toolbar
+
+   urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
